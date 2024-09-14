282af4b1509a0c7638f063deed0a6496fc173b63
Lidiya <simutina.1994@mail.ru>
2024-09-14 21:59:04 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point coordinate_1 = new Point(1, 3);
+        Point coordinate_2 = new Point3D(1, 3, 5);
+
+        System.out.println(coordinate_1);
+        System.out.println(coordinate_2);
+    }
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,34 @@
+public class Point {
+    int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
+class Point3D extends Point{
+    int z;
+
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
