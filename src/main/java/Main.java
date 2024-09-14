ea7d18f8d0ce34dbdf7c2646f3ecc3c2a557ef1c
Lidiya <simutina.1994@mail.ru>
2024-09-14 22:46:20 +0300

    Добавлено задание Полиморфизм #2. Замкнутая линия

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Point coordinate_1 = new Point(1, 3);
-        Point coordinate_2 = new Point3D(1, 3, 5);
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(2,1);
+        Point p3 = new Point(2,2);
+        Point p4 = new Point(1,2);
 
-        System.out.println(coordinate_1);
-        System.out.println(coordinate_2);
+        Point[] arrPoint = {p1, p2, p3, p4};
+
+        PolyLine polyLine = new PolyLine(arrPoint);
+
+        ClosedPolyLine closedPolyLine = new ClosedPolyLine(arrPoint);
+
+        System.out.println(polyLine.length());
+        System.out.println(closedPolyLine.closedLength());
+
     }
 }
---
+++ src/main/java/PolyLine.java
@@ -0,0 +1,38 @@
+public class PolyLine {
+    Point[] points;
+
+    public PolyLine(Point... arrPoints) {
+        this.points = arrPoints;
+    }
+
+    public double length() {
+        double sum = 0, len1, len2;
+        for (int i = 0; i < points.length - 1; i++) {
+            len1 = points[i].x - points[i + 1].x;
+            len2 = points[i].y - points[i + 1].y;
+            sum += Math.sqrt(len1 * len1 + len2 * len2);
+        }
+        return sum;
+    }
+}
+
+class ClosedPolyLine extends PolyLine {
+    public ClosedPolyLine(Point... points) {
+        super(points);
+    }
+
+    @Override
+    public double length() {
+        return super.length();
+    }
+
+    public double closedLength() {
+        double sum, len1, len2;
+        sum = length();
+        len1 = points[0].x - points[points.length - 1].x;
+        len2 = points[0].y - points[points.length - 1].y;
+        sum += Math.sqrt(len1 * len1 + len2 * len2);
+
+        return sum;
+    }
+}
