ed35586b0f91e68c9db62b502c0da06e487fea8a
Lidiya <simutina.1994@mail.ru>
2024-09-19 22:36:41 +0300

    Добавлено задание Полиморфизм #6. Сложение

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Fraction fraction = new Fraction(2,3);
-        System.out.println(fraction.toString());
-        System.out.println(fraction.intValue());
-        System.out.println(fraction.longValue());
-        System.out.println(fraction.floatValue());
-        System.out.println(fraction.doubleValue());
+        System.out.println(sum(2, new Fraction(3,5), 2.3));
+        System.out.println(sum(3.6, new Fraction(49,12), 3, new Fraction(3,2)));
+        System.out.println(sum(new Fraction(1,3), 1));
+    }
 
+    public static double sum(Number... numbers){
+        double result=0;
+        for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i].doubleValue();
+        }
+        return result;
     }
 
     static double mGetLength(LengthAble object) {
