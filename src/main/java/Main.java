9e8cd6da909b236248b72f63b03401b9d1a5880e
Lidiya <simutina.1994@mail.ru>
2024-09-19 23:23:43 +0300

    Добавлено задание Полиморфизм #7. Птицы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(sum(2, new Fraction(3,5), 2.3));
-        System.out.println(sum(3.6, new Fraction(49,12), 3, new Fraction(3,2)));
-        System.out.println(sum(new Fraction(1,3), 1));
-    }
+        Sparrow tom = new Sparrow("tom");
+        tom.sing();
 
-    public static double sum(Number... numbers){
-        double result=0;
-        for (int i = 0; i < numbers.length; i++) {
-            result += numbers[i].doubleValue();
-        }
-        return result;
+        Cuckoo kukusha = new Cuckoo("Kukusha");
+        kukusha.sing();
+
+        String text = "Карусель, карусель\n" +
+                "Начинает рассказ\n" +
+                "Это сказки, песни и веселье\n" +
+                "Карусель, карусель —\n" +
+                "Это радость для нас\n" +
+                "Прокатись на нашей карусели\n";
+        Parrot grisha = new Parrot("Grisha", text);
+        grisha.sing();
+
     }
 
     static double mGetLength(LengthAble object) {
