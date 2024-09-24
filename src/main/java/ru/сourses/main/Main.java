a8679855b19a65d46111a75a55a8304381860711
Lidiya <simutina.1994@mail.ru>
2024-09-24 22:59:43 +0300

    Добавлено задание Пакеты #4. Простые имена

--- src/main/java/ru/сourses/main/Main.java
+++ src/main/java/ru/сourses/main/Main.java
@@ -1,21 +1,19 @@
 package ru.сourses.main;
 
-import java.util.Scanner;
+import ru.сourses.geometry.Point;
 
+import java.util.Scanner;
 import static java.lang.Integer.parseInt;
 import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Вводим первое число");
-        String x = sc1.next();
-        Scanner sc2 = new Scanner(System.in);
-        System.out.println("Вводим второе число");
-        String y = sc2.next();
-        System.out.println(raisingToAPower(x,y));
+        Point point=new Point(1,2);
+        java.awt.Point p1 = new java.awt.Point(2,3);
 
+        System.out.println(point);
+        System.out.println(p1);
     }
 
     public static double raisingToAPower(String x, String y){
